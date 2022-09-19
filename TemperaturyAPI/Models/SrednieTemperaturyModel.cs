ba2b04e342c60f1c790c747e3f9f3f7752fb7e70
Mateusz Bryl <brylek@windowslive.com>
2022-09-19 17:18:03 +0200

    kolejny backup kodu

---
+++ TemperaturyAPI/Models/SrednieTemperaturyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace TemperaturyAPI.Models
+{
+    [Keyless]
+    public class SrednieTemperaturyModel
+    {
+        public DateOnly DataPomiaru { get; set; }
+        public float SredniaGleby { get; set; }
+        public float SredniaPowietrza { get; set; }
+    }
+    
+}
+
